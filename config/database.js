732a24de1ef04d4b849d0c829f591836ef671143
Mitchell <mitchell4408@hotmail.com>
2017-01-13 11:18:09 -0800

    fix bson bug

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'your-settings-here' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'mongodb://heroku_tz5zjsr1:777vhilog9pp0094469f6ghdm8@ds111239-a0.mlab.com:11239,ds111239-a1.mlab.com:11239/heroku_tz5zjsr1?replicaSet=rs-ds111239' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
