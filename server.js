fd401c5da3aaa2bd40da4cbed009b2cc8af4efc2
Mitchell <mitchell4408@hotmail.com>
2017-01-13 07:55:00 -0800

    routes

--- server.js
+++ server.js
@@ -1,5 +1,33 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash    = require('connect-flash');
+
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
+
+var configDB = require('./config/database.js');
+
+mongoose.connect(configDB.url); // connect to our database
+
+// require('./config/passport')(passport); // pass passport for configuration
+
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser()); // get information from html forms
+
+// required for passport
+app.use(session({ secret: 'waterassassins' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+// routes ======================================================================
+require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
 //setup port
 //process.env.PORT lets the port be set by heroku
@@ -10,24 +38,6 @@ app.set('view engine', 'ejs');
 
 //make express look in the public directory for assets (css/js/img)
 app.use(express.static(__dirname + '/public'));
-
-//set the home page route
-app.get('/', function(req, res) {
-  //ejs render automatically looks in the views folder
-  res.render('index');
-});
-
-app.get('/login', function(req, res) {
-  //TODO
-});
-
-app.get('/signup', function(req, res) {
-  //TODO
-});
-
-app.get('/profile', function(req, res) {
-  //TODO
-});
 
 app.listen(port, function() {
   console.log('our app is running on http://localhost' + port);
